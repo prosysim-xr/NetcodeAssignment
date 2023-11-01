a5efb0db566d8910f0f71bdaa0b5befe2d110c96
sumanks-xr <suman.singhxr@gmail.com>
2023-11-02 01:04:22 +0530

    Proper Model Allignment Done on clicking the panel QR

--- ARNetcodeProject/Assets/_Project/Scripts/GameManager.cs
+++ ARNetcodeProject/Assets/_Project/Scripts/GameManager.cs
@@ -1,27 +1,38 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     [SerializeField] GameObject player;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] GameObject modelToAllign;
+
+    [SerializeField] Color[] colorArray;
+    [SerializeField] Transform[] showCaseRoomQRArray;
+    [SerializeField] Transform[] modelTOAllignQRArray;
+
+    // colorToQRs is showCaseRoomQR and modelToAllignQR.
+    private Dictionary<Color, Tuple<Transform, Transform>> colorToQRs = new Dictionary<Color, Tuple<Transform, Transform>>();
 
 
     private void Start() {
-        
+        // Add all the colors and QRs to the dictionary
+        for (int i = 0; i < colorArray.Length; i++) {
+            colorToQRs.Add(colorArray[i], new Tuple<Transform, Transform>(showCaseRoomQRArray[i], modelTOAllignQRArray[i]));
+        }
     }
     private void Update() {
         // Ray Cast from crosshair position into z direction of camera
         Ray ray = Camera.main.ScreenPointToRay(crosshair.position);
         RaycastHit hit;
         // If ray hits something with box collider and the name of it is "Panel" then Debug.Log("Hit Panel")
+
         if (Input.GetMouseButtonDown(0)) { 
             if (Physics.Raycast(ray, out hit, 100f) && hit.collider.name == "Panel") {
-                Color colorOfPanelQR = hit.collider.GetComponent<MeshRenderer>().material.color;
-                Debug.Log($"Hit Panel and Color is {colorOfPanelQR}");
+                Color showCaseRoomQRColor = hit.collider.GetComponent<MeshRenderer>().material.color;
+                Debug.Log($"Hit Panel and Color is {showCaseRoomQRColor}");
 
+                AllignModel( showCaseRoomQRColor);
             }
         }
 
@@ -31,6 +42,27 @@ public class GameManager : MonoBehaviour
 
     }
 
+    void AllignModel(Color showCaseRoomQRColor) {
+        colorToQRs.TryGetValue(showCaseRoomQRColor, out var qrInfo);
+        Transform showCaseRoomQRTr = qrInfo.Item1;
+        Transform modelToAllignQRTr = qrInfo.Item2;
+        GameObject go = new GameObject("AllignmentModelTemp");
+        go.transform.parent = modelToAllignQRTr;
+        go.transform.localPosition = Vector3.zero;
+        go.transform.localRotation = Quaternion.identity;
+
+        go.transform.parent = showCaseRoomQRTr.root;
+        //go.transform.parent = null;
+
+        modelToAllign.transform.parent = go.transform;
+
+        go.transform.position = showCaseRoomQRTr.position;
+        go.transform.rotation = showCaseRoomQRTr.rotation;
+
+        modelToAllign.transform.parent = null;
+        Destroy(go);
+
+    }
 
     public void PanelData() { // use following to click and correctly orient once this is done Same Data can be sent to Multiplayer and they can update there model aswell.
         // Create a dictionary to hold the data
@@ -64,4 +96,6 @@ public class GameManager : MonoBehaviour
             // Now you have access to the data for the "red" panel.
         }
     }
+
+    
 }
